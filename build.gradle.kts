dd05630728321b4b0809f8301682636b7156e83b
Nikita Sahno <n.sahno@smartway.today>
2023-11-03 14:06:21 +0300

    bot init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.net.URI
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
@@ -8,12 +10,14 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven { url = URI.create("https://jitpack.io") }
 }
 
 dependencies {
     testImplementation(kotlin("test"))
     runtimeOnly("org.kodein.di:kodein-di:7.18.0")
     implementation("io.ktor:ktor-client-core:2.3.5")
+    implementation ("io.github.kotlin-telegram-bot.kotlin-telegram-bot:telegram:6.1.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,21 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.github.kotlintelegrambot.bot
+import com.github.kotlintelegrambot.dispatch
+import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.entities.ChatId
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val bot = bot {
+        token = "6597360611:AAFZQQfRVjVkOZAT1_Ux6z9atoCHeWNFqmo"
+        dispatch {
+            command("start") {
+                val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Hi there!")
+                result.fold({
+                    // do something here with the response
+                },{
+                    // do something with the error
+                })
+            }
+        }
+    }
+    bot.startPolling()
 }
