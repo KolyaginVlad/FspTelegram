6a18416c9d66036d4435db710bc79c06dd5330fb
Nikita Sahno <n.sahno@smartway.today>
2023-11-03 14:25:35 +0300

    bot init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 import com.github.kotlintelegrambot.bot
 import com.github.kotlintelegrambot.dispatch
 import com.github.kotlintelegrambot.dispatcher.command
+import com.github.kotlintelegrambot.dispatcher.pollAnswer
 import com.github.kotlintelegrambot.entities.ChatId
 
 fun main() {
@@ -11,9 +12,18 @@ fun main() {
                 val result = bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Hi there!")
                 result.fold({
                     // do something here with the response
-                },{
+                }, {
                     // do something with the error
                 })
+            }
+            command("enter_server_data") {
+                bot.sendMessage(chatId = ChatId.fromId(message.chat.id), text = "Enter server data!")
+                pollAnswer {
+
+                    bot.sendMessage(
+                        chatId = ChatId.fromId(message.chat.id), text = update.message.toString()
+                    )
+                }
             }
         }
     }
